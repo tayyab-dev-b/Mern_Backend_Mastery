77bb403dabf456820c0bc9e0a6a6409e927a95fc
Tayyab khan <tk7635373@gmail.com>
2026-04-18 00:19:13 +0700

    Added 02_modules folder

---
+++ 02_modules/index.js
@@ -0,0 +1,23 @@
+// const math = require('./math');
+
+// console.log(math.add(3,5));
+// console.log(math.sub(3,5));
+
+// file handelling using fs module :
+
+const fs = require('fs');
+
+fs.writeFile('hello.txt', 'hello world', (err)=>{
+    if (err) throw err;
+    console.log('file created');
+})
+
+fs.readFile('hello.txt', 'utf-8' ,(err, data)=>{
+    if (err) throw err;
+    console.log(data);
+})
+
+fs.appendFile('hello.txt', '\n welcome to node js,update the file',(err)=>{
+    if (err) throw err;
+    console.log('file updated');
+})
